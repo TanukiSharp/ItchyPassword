255702b6196663df0977ba683545e4b44c2430c7
Sebastien ROBERT <sebastien.saigo@gmail.com>
2019-10-08 23:30:10 +0900

    Improved frontend UI, added confirmation box for private part

--- docs/ui.js
+++ docs/ui.js
@@ -1,5 +1,9 @@
 const txtPrivatePart = document.getElementById('txtPrivatePart');
+const txtPrivatePartConfirmation = document.getElementById('txtPrivatePartConfirmation');
 const txtPublicPart = document.getElementById('txtPublicPart');
+
+const spnPrivatePartSize = document.getElementById('spnPrivatePartSize');
+const spnPrivatePartSizeConfirmation = document.getElementById('spnPrivatePartSizeConfirmation');
 
 const numOutputSizeRange = document.getElementById('numOutputSizeRange');
 const numOutputSizeNum = document.getElementById('numOutputSizeNum');
@@ -53,17 +57,48 @@ btnResetAlphabet.addEventListener('click', () => {
 updateOutputSizeRangeToNum();
 resetAlphabet();
 
+const clearOutputs = () => {
+    txtResultB16.value = '';
+    txtResultB64.value = '';
+    txtResultCustomBase.value = '';
+};
+
 const run = async () => {
-    if (txtPublicPart.value.length <= 0) {
+    const privatePart = txtPrivatePart.value;
+    const publicPart = txtPublicPart.value;
+
+    checkPrivatePartsMatching();
+
+    if (privatePart.length <= 0 || publicPart.length <= 0) {
+        clearOutputs();
         return;
     }
 
-    const keyBytes = await generatePassword(txtPrivatePart.value, txtPublicPart.value);
+    const keyBytes = await generatePassword(privatePart, publicPart);
 
     txtResultB16.value = truncate(bufferToHexadeximal(keyBytes), numOutputSizeRange.value);
     txtResultB64.value = truncate(bufferToBase64(keyBytes), numOutputSizeRange.value);
     txtResultCustomBase.value = truncate(toCustomBase(keyBytes, txtCustomAlphabet.value), numOutputSizeRange.value);
 };
 
-txtPrivatePart.addEventListener('input', run);
+txtPrivatePart.addEventListener('input', () => {
+    spnPrivatePartSize.innerText = txtPrivatePart.value.length;
+    run();
+});
+
+const checkPrivatePartsMatching = () => {
+    if (txtPrivatePartConfirmation.value === txtPrivatePart.value) {
+        txtPrivatePartConfirmation.style.setProperty('background', '#D0FFD0');
+    } else {
+        txtPrivatePartConfirmation.style.setProperty('background', '#FFD0D0');
+    }
+};
+
+txtPrivatePartConfirmation.addEventListener('input', () => {
+    spnPrivatePartSizeConfirmation.innerText = txtPrivatePartConfirmation.value.length;
+    run();
+});
+
+checkPrivatePartsMatching();
+
 txtPublicPart.addEventListener('input', run);
