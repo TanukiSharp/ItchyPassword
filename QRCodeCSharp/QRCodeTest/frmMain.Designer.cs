769afbd79c8fa264bea111c6e787e81c7b7cdab3
Sebastien ROBERT <sebastien.saigo@gmail.com>
2019-11-03 10:27:58 +0900

    First commit to port C# code to TypeScript

---
+++ QRCodeCSharp/QRCodeTest/frmMain.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace QRCodeTest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEncodeData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picEncode = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picEncode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSize
+            // 
+            this.txtSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtSize.Location = new System.Drawing.Point(47, 425);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(43, 20);
+            this.txtSize.TabIndex = 8;
+            this.txtSize.Text = "5";
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnEncode.Location = new System.Drawing.Point(12, 452);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(94, 27);
+            this.btnEncode.TabIndex = 1;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Size";
+            // 
+            // txtEncodeData
+            // 
+            this.txtEncodeData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEncodeData.Location = new System.Drawing.Point(47, 398);
+            this.txtEncodeData.Name = "txtEncodeData";
+            this.txtEncodeData.Size = new System.Drawing.Size(688, 20);
+            this.txtEncodeData.TabIndex = 2;
+            this.txtEncodeData.Text = "test en mousse";
+            this.txtEncodeData.TextChanged += new System.EventHandler(this.txtEncodeData_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 401);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data";
+            // 
+            // picEncode
+            // 
+            this.picEncode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picEncode.BackColor = System.Drawing.Color.White;
+            this.picEncode.Location = new System.Drawing.Point(12, 13);
+            this.picEncode.Name = "picEncode";
+            this.picEncode.Size = new System.Drawing.Size(723, 378);
+            this.picEncode.TabIndex = 0;
+            this.picEncode.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 492);
+            this.Controls.Add(this.picEncode);
+            this.Controls.Add(this.txtEncodeData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.label4);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QRCode Tester";
+            ((System.ComponentModel.ISupportInitialize)(this.picEncode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+		private System.Windows.Forms.PictureBox picEncode;
+		private System.Windows.Forms.TextBox txtEncodeData;
+        private System.Windows.Forms.TextBox txtSize;
+		private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnEncode;
+    }
+}
+
---
+++ QRCodeCSharp/QRCodeTest/frmMain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using QRCode;
+
+namespace QRCodeTest
+{
+	public partial class frmMain : Form
+	{
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+        public static Bitmap Encode(int scale, byte[] content)
+        {
+            bool[][] matrix = QRCodeEncoder.ComputeQRCode(content);
+
+            var brush = new SolidBrush(Color.White);
+            var image = new Bitmap(matrix.Length * scale, matrix.Length * scale);
+            var g = Graphics.FromImage(image);
+
+            g.FillRectangle(brush, new Rectangle(0, 0, image.Width, image.Height));
+
+            brush.Color = Color.Black;
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    if (matrix[j][i])
+                        g.FillRectangle(brush, j * scale, i * scale, scale, scale);
+                }
+            }
+
+            return image;
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+		{
+			if (txtEncodeData.Text.Trim().Length == 0)
+			{
+				MessageBox.Show("Data must not be empty.");
+				return;
+			}
+
+			if (ushort.TryParse(txtSize.Text, out ushort scale) == false || scale > 20)
+			{
+				MessageBox.Show("Invalid size!");
+				return;
+			}
+
+			byte[] buffer = Encoding.UTF8.GetBytes(txtEncodeData.Text);
+
+			picEncode.Image = Encode(scale, buffer);
+		}
+
+        private void txtEncodeData_TextChanged(object sender, EventArgs e)
+        {
+            if (txtEncodeData.Text.Trim().Length == 0)
+            {
+                picEncode.Image = null;
+                return;
+            }
+
+            if (ushort.TryParse(txtSize.Text, out ushort scale) == false || scale > 20)
+            {
+                picEncode.Image = null;
+                return;
+            }
+
+            byte[] buffer = Encoding.UTF8.GetBytes(txtEncodeData.Text);
+
+            picEncode.Image = Encode(scale, buffer);
+        }
+    }
+}
