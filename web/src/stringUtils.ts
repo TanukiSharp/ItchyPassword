51179d8b9c7d36c4fe3b4c64ddcc48ef29935c66
Sebastien ROBERT <sebastien.saigo@gmail.com>
2020-07-30 18:41:48 +0900

    Added stringUtils.trim function

--- web/src/stringUtils.ts
+++ web/src/stringUtils.ts
@@ -1,3 +1,30 @@
+function findNonCharFromStart(value: string, chars: string[]): number {
+    for (let i = 0; i < value.length; i += 1) {
+        if (chars.includes(value[i]) === false) {
+            return i;
+        }
+    }
+
+    return value.length;
+}
+
+function findNonCharFromEnd(value: string, chars: string[]): number {
+    for (let i = value.length - 1; i >= 0; i -= 1) {
+        if (chars.includes(value[i]) === false) {
+            return i + 1;
+        }
+    }
+
+    return value.length;
+}
+
+export function trim(value: string, ...chars: string[]): string {
+    const start: number = findNonCharFromStart(value, chars);
+    const end: number = findNonCharFromEnd(value, chars);
+
+    return value.substring(start, end);
+}
+
 export function truncate(input: string, length: number): string {
     if (input.length <= length) {
         return input;
