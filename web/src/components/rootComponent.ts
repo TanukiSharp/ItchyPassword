c63774a0ed23d6f94950737825dbaccc906f36e4
Sebastien ROBERT <sebastien.saigo@gmail.com>
2020-07-30 18:43:13 +0900

    Moved code from index.ts to a RootComponent class

---
+++ web/src/components/rootComponent.ts
@@ -0,0 +1,64 @@
+import { getElementById } from '../ui';
+import { TabControl, ITabInfo } from '../TabControl';
+
+import { IComponent } from './IComponent';
+import { PrivatePartComponent } from './privatePartComponent';
+import { PasswordComponent } from './passwordComponent';
+import { CipherComponent } from './cipherComponent';
+import { ReEncryptComponent } from './reEncryptComponent';
+import { VaultComponent } from './vaultComponent';
+
+import * as storageOutputComponent from './storageOutputComponent';
+
+const nothingTabInfo: ITabInfo = {
+    getTabButton(): HTMLButtonElement {
+        return getElementById('btnTabNothing') as HTMLButtonElement;
+    },
+    getTabContent(): HTMLElement {
+        return getElementById('divTabNothing');
+    },
+    onTabSelected(): void {
+        storageOutputComponent.hide();
+    }
+}
+
+const elements: any[] = [
+    nothingTabInfo,
+    new PrivatePartComponent(),
+    new PasswordComponent(),
+    new CipherComponent(),
+    new ReEncryptComponent(),
+    new storageOutputComponent.StorageOutputComponent(),
+    new VaultComponent(),
+];
+
+const tabs: ITabInfo[] = elements.filter(e => (e as ITabInfo).getTabButton !== undefined);
+const components: IComponent[] = elements.filter(e => (e as IComponent).init !== undefined);
+
+const tabControl = new TabControl(tabs);
+
+export class RootComponent implements IComponent {
+    public readonly name: string = 'Root';
+
+    public constructor() {
+    }
+
+    public init(): void {
+        let component: IComponent;
+        for (component of components) {
+            component.init();
+        }
+    }
+
+    public getActiveComponent(): IComponent | null {
+        const component = tabs[tabControl.activeTabIndex] as any;
+
+        if (component.init !== undefined) {
+            return component as IComponent;
+        }
+
+        return null;
+    }
+}
+
+export const rootComponent = new RootComponent();
--- web/src/index.ts
+++ web/src/index.ts
@@ -1,41 +1,5 @@
 import { getElementById } from './ui';
-import { TabControl, ITabInfo } from './TabControl';
-import { IComponent } from './components/IComponent';
-
-import { PrivatePartComponent } from './components/privatePartComponent';
-import { PasswordComponent } from './components/passwordComponent';
-import { CipherComponent } from './components/cipherComponent';
-import { ReEncryptComponent } from './components/reEncryptComponent';
-import { VaultComponent } from './components/vaultComponent';
-
-import * as storageOutputComponent from './components/storageOutputComponent';
-
-const nothingTabInfo: ITabInfo = {
-    getTabButton(): HTMLButtonElement {
-        return getElementById('btnTabNothing') as HTMLButtonElement;
-    },
-    getTabContent(): HTMLElement {
-        return getElementById('divTabNothing');
-    },
-    onTabSelected(): void {
-        storageOutputComponent.hide();
-    }
-}
-
-const elements: any[] = [
-    nothingTabInfo,
-    new PrivatePartComponent(),
-    new PasswordComponent(),
-    new CipherComponent(),
-    new ReEncryptComponent(),
-    new storageOutputComponent.StorageOutputComponent(),
-    new VaultComponent(),
-];
-
-const tabs: ITabInfo[] = elements.filter(e => (e as ITabInfo).getTabButton !== undefined);
-const components: IComponent[] = elements.filter(e => (e as IComponent).init !== undefined);
-
-new TabControl(tabs);
+import { rootComponent } from './components/rootComponent';
 
 declare const COMMITHASH: string;
 
@@ -44,7 +8,4 @@ const githubLink = '<a href="https://github.com/TanukiSharp/ItchyPassword" targe
 
 getElementById('divInfo').innerHTML = `${version}<br/>${githubLink}`;
 
-let component: IComponent;
-for (component of components) {
-    component.init();
-}
+rootComponent.init();
