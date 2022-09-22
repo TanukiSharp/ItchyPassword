3fb0ec0a91328e3bcb1221002577a22767407800
Sebastien ROBERT <sebastien.saigo@gmail.com>
2022-09-23 07:58:04 +0900

    Simplified single file encryption/decryption tool

--- cli/ItchyPassword.SingleFile/Program.cs
+++ cli/ItchyPassword.SingleFile/Program.cs
@@ -2,46 +2,37 @@
 using System.Text;
 using ItchyPassword.Core;
 
-const int Encrypted = 1;
-const int Clear = 2;
+const int Encrypt = 1;
+const int Decrypt = 2;
 
-if (args.Length == 0)
+if (args.Length != 1)
 {
-    Console.WriteLine("Pass a file as argument, empty file to start.");
+    Console.WriteLine("Pass one (and only one) file as argument.");
     return -1;
 }
 
-if (args.Length > 1)
-{
-    Console.WriteLine("Pass only one file as argument, empty file to start.");
-    return -1;
-}
-
-Console.WriteLine("Is file content encrypted ?");
-Console.WriteLine("1. Yes (default)");
-Console.WriteLine("2. No");
+Console.WriteLine($"Do you want to encrypt or decrypt the file ? [{args[0]}]");
+Console.WriteLine("1. Encrypt");
+Console.WriteLine("2. Decrypt");
 Console.WriteLine();
 Console.Write("> ");
 
 string? response = Console.ReadLine();
 
-if (response == null)
+if (string.IsNullOrWhiteSpace(response))
 {
     Console.WriteLine("Invalid input, exiting.");
     return -2;
 }
 
-if (response == "")
-    response = Encrypted.ToString();
-
-if (int.TryParse(response, out int contentType) == false || contentType < Encrypted || contentType > Clear)
+if (int.TryParse(response, out int operationType) == false || operationType < Encrypt || operationType > Decrypt)
 {
     Console.WriteLine("Invalid input, exiting.");
     return -2;
 }
 
 byte[] fileContent = File.ReadAllBytes(args[0]);
-byte[] clearData;
+byte[] resultData;
 
 Console.Write("Input master key: ");
 Console.Out.Flush();
@@ -63,44 +54,19 @@ Console.WriteLine();
 Console.WriteLine($"Master key is {masterKey.Length} characters long.");
 
 string ext = Path.GetExtension(args[0]);
-string workingFile = Path.ChangeExtension(args[0], $".tmp{ext}");
+string workingFile = Path.ChangeExtension(args[0], $".output{ext}");
 
-if (contentType == Encrypted)
+if (operationType == Encrypt)
 {
-    Console.WriteLine("Decrypting file content...");
-    clearData = Crypto.Decrypt(fileContent, Encoding.UTF8.GetBytes(masterKey));
+    Console.WriteLine($"Encrypting file content... [{workingFile}]");
+    resultData = Crypto.Encrypt(fileContent, Encoding.UTF8.GetBytes(masterKey));
 }
 else
 {
-    clearData = fileContent;
+    Console.WriteLine($"Decrypting file content... [{workingFile}]");
+    resultData = Crypto.Decrypt(fileContent, Encoding.UTF8.GetBytes(masterKey));
 }
 
-File.WriteAllBytes(workingFile, clearData);
-
-Process? p = Process.Start(new ProcessStartInfo(workingFile)
-{
-    UseShellExecute = true
-});
-
-if (p == null)
-{
-    Console.WriteLine("Failed to run input file.");
-    return -1;
-}
-
-Console.WriteLine("Opened file with application.");
-p.WaitForExit();
-Console.WriteLine("Application closed.");
-
-fileContent = File.ReadAllBytes(workingFile);
-
-Console.WriteLine("Encrypting file content...");
-
-byte[] encryptedData = Crypto.Encrypt(fileContent, Encoding.UTF8.GetBytes(masterKey));
-
-File.WriteAllBytes(args[0], encryptedData);
-File.Delete(workingFile);
-
-Console.WriteLine("File protected.");
+File.WriteAllBytes(workingFile, resultData);
 
 return 0;
