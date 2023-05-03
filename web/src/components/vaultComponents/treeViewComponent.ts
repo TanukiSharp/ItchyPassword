3e1851fe4ba489397b12f02d4930900de0a66842
Sebastien ROBERT <sebastien.saigo@gmail.com>
2023-05-03 16:59:58 +0900

    Added password and cipher lifetime display in tree view (color in button)

--- web/src/components/vaultComponents/treeViewComponent.ts
+++ web/src/components/vaultComponents/treeViewComponent.ts
@@ -10,6 +10,8 @@ import * as serviceManager from '../../services/serviceManger';
 import { PasswordService } from '../../services/passwordService';
 import { CipherService } from 'services/cipherService';
 
+const ONE_YEAR_IN_MILLISECONDS = 365 * 24 * 3600 * 1000;
+
 const btnTabVaultTabTreeView = getElementById('btnTabVaultTabTreeView') as HTMLButtonElement;
 const divTabVaultTabTreeView = getElementById('divTabVaultTabTreeView');
 
@@ -54,10 +56,15 @@ function populateSearchFunctions(): void {
 class VaultTreeNodeCreationController implements TreeNodeCreationController {
     private readonly passwordService: PasswordService;
     private readonly cipherService: CipherService;
+    private readonly buttonBackgroundColor: string;
 
-    public constructor() {
+    public constructor(private readonly maxTimestampInMilliseconds: number) {
         this.passwordService = serviceManager.getService('password');
         this.cipherService = serviceManager.getService('cipher');
+
+        const buttonStyle = window.getComputedStyle(btnTabVaultTabTreeView);
+
+        this.buttonBackgroundColor = buttonStyle.backgroundColor;
     }
 
     private async runPassword(value: any): Promise<void> {
@@ -139,15 +146,54 @@ class VaultTreeNodeCreationController implements TreeNodeCreationController {
         return plainObject.isPlainObject(value) === false;
     }
 
-    createTreeNodeContentElement(path: string, key: string, value: any): HTMLElement {
+    private computeTimestampProgress(timestamp: Date): number {
+        const now = Date.now()
+        const then = timestamp.getTime();
+        const diff = now - then;
+
+        const timespan = Math.max(0, Math.min(diff, this.maxTimestampInMilliseconds));
+
+        return timespan / this.maxTimestampInMilliseconds;
+    }
+
+    private static dateToString(date: Date): string {
+        return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    }
+
+    private createButton(text: string, timestamp: Date) {
+        const button = document.createElement('button');
+
+        button.title = `Last modified: ${VaultTreeNodeCreationController.dateToString(timestamp)}`;
+
+        const grid = document.createElement('div');
+        grid.classList.add('content-container');
+
+        const textSpan = document.createElement('span');
+        textSpan.innerText = text;
+        textSpan.classList.add('text');
+        grid.appendChild(textSpan);
+
+        const timestampDiv = document.createElement('div');
+        timestampDiv.classList.add('timestamp');
+        timestampDiv.style.backgroundColor = this.buttonBackgroundColor;
+        timestampDiv.style.width = `${Math.round((1 - this.computeTimestampProgress(timestamp)) * 100)}%`;
+        grid.appendChild(timestampDiv);
+
+        button.appendChild(grid);
+
+        return button;
+    }
+
+    public createTreeNodeContentElement(path: string, key: string, value: any): HTMLElement {
         if (VaultTreeNodeCreationController.isPasswordObject(key, value)) {
             const version: number = value.version;
             const isLatest = this.passwordService.isLatestVersion(version);
 
-            const button = document.createElement('button');
+            const lastModified = new Date(value.datetime);
+
+            const button = this.createButton('Password', lastModified);
             button.style.justifySelf = 'start';
             button.style.minWidth = '80px';
-            button.innerText = 'Password';
 
             if (isLatest === false) {
                 button.setAttribute('not-latest', `⚠️ Password version ${version}, latest is ${this.passwordService.getLatestVersion()}`);
@@ -164,10 +210,10 @@ class VaultTreeNodeCreationController implements TreeNodeCreationController {
             const version: number = value.version;
             const isLatest = this.cipherService.isLatestVersion(version);
 
-            const button = document.createElement('button');
+            const lastModified = new Date(value.datetime);
+
+            const button = this.createButton(key, lastModified);
             button.style.justifySelf = 'start';
-            button.innerText = key;
-            button.title = 'Open in ciphers';
 
             if (isLatest === false) {
                 button.setAttribute('not-latest', `⚠️ Cipher version ${version}, latest is ${this.cipherService.getLatestVersion()}`);
@@ -198,7 +244,9 @@ export class VaultTreeViewComponent implements IComponent, ITabInfo, IVaultCompo
     public readonly name: string = 'VaultTreeView';
 
     public onVaultLoaded(vault: plainObject.PlainObject): void {
-        rootTreeNode = new TreeNode(null, '<root>', '', vault, new VaultTreeNodeCreationController());
+        const maxTimespan = 3 * ONE_YEAR_IN_MILLISECONDS;
+
+        rootTreeNode = new TreeNode(null, '<root>', '', vault, new VaultTreeNodeCreationController(maxTimespan));
 
         trvVaultTreeView.innerHTML = '';
         trvVaultTreeView.appendChild(rootTreeNode.element);
