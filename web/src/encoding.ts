e2989244fc893dd6d6d3d83181b203b32643f33b
Sebastien ROBERT <sebastien.saigo@gmail.com>
2023-05-15 22:53:23 +0900

    Added Base64 encoding

--- web/src/encoding.ts
+++ web/src/encoding.ts
@@ -47,6 +47,8 @@ export class Base62Encoding implements IEncoding {
     }
 }
 
+// Code to encode and decode base64 is taken from MDN and adjusted.
+
 export class Base64Encoding implements IEncoding {
     get name(): string {
         return 'base64';
@@ -57,231 +59,117 @@ export class Base64Encoding implements IEncoding {
     }
 
     encode(input: ArrayBuffer): string {
-        return this._utf8ArrToStr(new Uint8Array(input))
+        return this._base64Encode(new Uint8Array(input))
     }
 
     decode(input: string): ArrayBuffer {
-        return this._strToUTF8Arr(input).buffer;
+        return this._base64Decode(input).buffer;
     }
 
-    // _b64ToUint6(nChr: number): number {
-    //     return nChr > 64 && nChr < 91
-    //         ? nChr - 65
-    //         : nChr > 96 && nChr < 123
-    //             ? nChr - 71
-    //             : nChr > 47 && nChr < 58
-    //                 ? nChr + 4
-    //                 : nChr === 43
-    //                     ? 62
-    //                     : nChr === 47
-    //                         ? 63
-    //                         : 0;
-    // }
-
-    // _base64DecToArr(sBase64: string, nBlocksSize?: number): Uint8Array {
-    //     const sB64Enc = sBase64.replace(/[^A-Za-z0-9+/]/g, ''); // Remove any non-base64 characters, such as trailing '=', whitespace, and more.
-    //     const nInLen = sB64Enc.length;
-    //     const nOutLen = nBlocksSize
-    //         ? Math.ceil(((nInLen * 3 + 1) >> 2) / nBlocksSize) * nBlocksSize
-    //         : (nInLen * 3 + 1) >> 2;
-    //     const taBytes = new Uint8Array(nOutLen);
-
-    //     let nMod3;
-    //     let nMod4;
-    //     let nUint24 = 0;
-    //     let nOutIdx = 0;
-    //     for (let nInIdx = 0; nInIdx < nInLen; nInIdx++) {
-    //         nMod4 = nInIdx & 3;
-    //         nUint24 |= this._b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << (6 * (3 - nMod4));
-    //         if (nMod4 === 3 || nInLen - nInIdx === 1) {
-    //             nMod3 = 0;
-    //             while (nMod3 < 3 && nOutIdx < nOutLen) {
-    //                 taBytes[nOutIdx] = (nUint24 >>> ((16 >>> nMod3) & 24)) & 255;
-    //                 nMod3++;
-    //                 nOutIdx++;
-    //             }
-    //             nUint24 = 0;
-    //         }
-    //     }
-
-    //     return taBytes;
-    // }
-
-    // /* Base64 string to array encoding */
-    // _uint6ToB64(nUint6: number): number {
-    //     return nUint6 < 26
-    //         ? nUint6 + 65
-    //         : nUint6 < 52
-    //             ? nUint6 + 71
-    //             : nUint6 < 62
-    //                 ? nUint6 - 4
-    //                 : nUint6 === 62
-    //                     ? 43
-    //                     : nUint6 === 63
-    //                         ? 47
-    //                         : 65;
-    // }
-
-    // _base64EncArr(aBytes: Uint8Array): string {
-    //     let nMod3 = 2;
-    //     let sB64Enc = '';
-
-    //     const nLen = aBytes.length;
-    //     let nUint24 = 0;
-    //     for (let nIdx = 0; nIdx < nLen; nIdx++) {
-    //         nMod3 = nIdx % 3;
-    //         // To break your base64 into several 80-character lines, add:
-    //         //   if (nIdx > 0 && ((nIdx * 4) / 3) % 76 === 0) {
-    //         //      sB64Enc += '\r\n';
-    //         //    }
-
-    //         nUint24 |= aBytes[nIdx] << ((16 >>> nMod3) & 24);
-    //         if (nMod3 === 2 || aBytes.length - nIdx === 1) {
-    //             sB64Enc += String.fromCodePoint(
-    //                 this._uint6ToB64((nUint24 >>> 18) & 63),
-    //                 this._uint6ToB64((nUint24 >>> 12) & 63),
-    //                 this._uint6ToB64((nUint24 >>> 6) & 63),
-    //                 this._uint6ToB64(nUint24 & 63)
-    //             );
-    //             nUint24 = 0;
-    //         }
-    //     }
-    //     return (
-    //         sB64Enc.substring(0, sB64Enc.length - 2 + nMod3) +
-    //         (nMod3 === 2 ? '' : nMod3 === 1 ? '=' : '==')
-    //     );
-    // }
-
-    /* UTF-8 array to JS string and vice versa */
-
-    _utf8ArrToStr(aBytes: Uint8Array): string {
-        let sView = '';
-        let nPart;
-        const nLen = aBytes.length;
-        for (let nIdx = 0; nIdx < nLen; nIdx++) {
-            nPart = aBytes[nIdx];
-            sView += String.fromCodePoint(
-                nPart > 251 && nPart < 254 && nIdx + 5 < nLen /* six bytes */
-                    ? /* (nPart - 252 << 30) may be not so safe in ECMAScript! So…: */
-                    (nPart - 252) * 1073741824 +
-                    ((aBytes[++nIdx] - 128) << 24) +
-                    ((aBytes[++nIdx] - 128) << 18) +
-                    ((aBytes[++nIdx] - 128) << 12) +
-                    ((aBytes[++nIdx] - 128) << 6) +
-                    aBytes[++nIdx] -
-                    128
-                    : nPart > 247 && nPart < 252 && nIdx + 4 < nLen /* five bytes */
-                        ? ((nPart - 248) << 24) +
-                        ((aBytes[++nIdx] - 128) << 18) +
-                        ((aBytes[++nIdx] - 128) << 12) +
-                        ((aBytes[++nIdx] - 128) << 6) +
-                        aBytes[++nIdx] -
-                        128
-                        : nPart > 239 && nPart < 248 && nIdx + 3 < nLen /* four bytes */
-                            ? ((nPart - 240) << 18) +
-                            ((aBytes[++nIdx] - 128) << 12) +
-                            ((aBytes[++nIdx] - 128) << 6) +
-                            aBytes[++nIdx] -
-                            128
-                            : nPart > 223 && nPart < 240 && nIdx + 2 < nLen /* three bytes */
-                                ? ((nPart - 224) << 12) +
-                                ((aBytes[++nIdx] - 128) << 6) +
-                                aBytes[++nIdx] -
-                                128
-                                : nPart > 191 && nPart < 224 && nIdx + 1 < nLen /* two bytes */
-                                    ? ((nPart - 192) << 6) + aBytes[++nIdx] - 128
-                                    : /* nPart < 127 ? */ /* one byte */
-                                    nPart
-            );
-        }
-        return sView;
+    _base64ToUint6(base64Char: number): number {
+        return base64Char > 64 && base64Char < 91
+            ? base64Char - 65
+            : base64Char > 96 && base64Char < 123
+                ? base64Char - 71
+                : base64Char > 47 && base64Char < 58
+                    ? base64Char + 4
+                    : base64Char === 43
+                        ? 62
+                        : base64Char === 47
+                            ? 63
+                            : 0;
     }
 
-    _strToUTF8Arr(sDOMStr: string) {
-        let aBytes;
-        let nChr: number | undefined;
-        const nStrLen = sDOMStr.length;
-        let nArrLen = 0;
+    _base64Decode(base64Input: string, blocksSize?: number): Uint8Array {
+        // Remove any non-base64 characters, such as trailing '=', whitespace, and more.
+        base64Input = base64Input.replace(/[^A-Za-z0-9+/]/g, '');
 
-        /* mapping… */
-        for (let nMapIdx = 0; nMapIdx < nStrLen; nMapIdx++) {
-            nChr = sDOMStr.codePointAt(nMapIdx);
+        const inputLength = base64Input.length;
+        const outputLength = blocksSize
+            ? Math.ceil(((inputLength * 3 + 1) >> 2) / blocksSize) * blocksSize
+            : (inputLength * 3 + 1) >> 2;
 
-            if (nChr === undefined) {
-                throw new Error(`Invalid index nMapIdx ${nMapIdx} in sDOMStr '${sDOMStr}'.`);
+        const outputBytes = new Uint8Array(outputLength);
+
+        let value24Bits = 0;
+
+        let outputIndex = 0;
+
+        for (let inputIndex = 0; inputIndex < inputLength; inputIndex++) {
+            const inputIndexMod4 = inputIndex % 4;
+
+            value24Bits |= this._base64ToUint6(base64Input.charCodeAt(inputIndex)) << (6 * (3 - inputIndexMod4));
+
+            if (inputIndexMod4 === 3 || inputLength - inputIndex === 1) {
+                let inputIndexMod3 = 0;
+
+                while (inputIndexMod3 < 3 && outputIndex < outputLength) {
+                    outputBytes[outputIndex] = (value24Bits >>> ((0b1_0000 >>> inputIndexMod3) & 0b1_1000)) & 0xFF;
+                    inputIndexMod3++;
+                    outputIndex++;
+                }
+
+                value24Bits = 0;
             }
-
-            if (nChr >= 0x10000) {
-                nMapIdx++;
-            }
-
-            nArrLen +=
-                nChr < 0x80
-                    ? 1
-                    : nChr < 0x800
-                        ? 2
-                        : nChr < 0x10000
-                            ? 3
-                            : nChr < 0x200000
-                                ? 4
-                                : nChr < 0x4000000
-                                    ? 5
-                                    : 6;
         }
 
-        aBytes = new Uint8Array(nArrLen);
+        return outputBytes;
+    }
 
-        /* transcription… */
-        let nIdx = 0;
-        let nChrIdx = 0;
-        while (nIdx < nArrLen) {
-            nChr = sDOMStr.codePointAt(nChrIdx);
+    /* Base64 string to array encoding */
+    _uint6ToBase64(value6Bits: number): number {
+        return value6Bits < 26
+            ? value6Bits + 65
+            : value6Bits < 52
+                ? value6Bits + 71
+                : value6Bits < 62
+                    ? value6Bits - 4
+                    : value6Bits === 62
+                        ? 43
+                        : value6Bits === 63
+                            ? 47
+                            : 65;
+    }
 
-            if (nChr === undefined) {
-                throw new Error(`Invalid index nMapIdx ${nChrIdx} in sDOMStr '${sDOMStr}'.`);
+    _base64Encode(inputBytes: Uint8Array): string {
+        let inputIndexMod3 = 2;
+        let base64Output = '';
+
+        const inputBytesLength = inputBytes.length;
+
+        let value24Bits = 0;
+
+        for (let inputIndex = 0; inputIndex < inputBytesLength; inputIndex++) {
+            inputIndexMod3 = inputIndex % 3;
+
+            // To break your base64 into several 80-character lines, add:
+            // if (inputIndex > 0 && ((inputIndex * 4) / 3) % 76 === 0) {
+            //     base64Output += '\n';
+            // }
+
+            const shift = 0b1_0000 >>> inputIndexMod3;
+            const constrainedShift = shift & 0b1_1000;
+
+            value24Bits |= inputBytes[inputIndex] << constrainedShift;
+
+            if (inputIndexMod3 === 2 || inputBytes.length - inputIndex === 1) {
+                base64Output += String.fromCodePoint(
+                    this._uint6ToBase64((value24Bits >>> 18) & 63),
+                    this._uint6ToBase64((value24Bits >>> 12) & 63),
+                    this._uint6ToBase64((value24Bits >>> 6) & 63),
+                    this._uint6ToBase64(value24Bits & 63)
+                );
+                value24Bits = 0;
             }
-
-            if (nChr < 128) {
-                /* one byte */
-                aBytes[nIdx++] = nChr;
-            } else if (nChr < 0x800) {
-                /* two bytes */
-                aBytes[nIdx++] = 192 + (nChr >>> 6);
-                aBytes[nIdx++] = 128 + (nChr & 63);
-            } else if (nChr < 0x10000) {
-                /* three bytes */
-                aBytes[nIdx++] = 224 + (nChr >>> 12);
-                aBytes[nIdx++] = 128 + ((nChr >>> 6) & 63);
-                aBytes[nIdx++] = 128 + (nChr & 63);
-            } else if (nChr < 0x200000) {
-                /* four bytes */
-                aBytes[nIdx++] = 240 + (nChr >>> 18);
-                aBytes[nIdx++] = 128 + ((nChr >>> 12) & 63);
-                aBytes[nIdx++] = 128 + ((nChr >>> 6) & 63);
-                aBytes[nIdx++] = 128 + (nChr & 63);
-                nChrIdx++;
-            } else if (nChr < 0x4000000) {
-                /* five bytes */
-                aBytes[nIdx++] = 248 + (nChr >>> 24);
-                aBytes[nIdx++] = 128 + ((nChr >>> 18) & 63);
-                aBytes[nIdx++] = 128 + ((nChr >>> 12) & 63);
-                aBytes[nIdx++] = 128 + ((nChr >>> 6) & 63);
-                aBytes[nIdx++] = 128 + (nChr & 63);
-                nChrIdx++;
-            } /* if (nChr <= 0x7fffffff) */ else {
-                /* six bytes */
-                aBytes[nIdx++] = 252 + (nChr >>> 30);
-                aBytes[nIdx++] = 128 + ((nChr >>> 24) & 63);
-                aBytes[nIdx++] = 128 + ((nChr >>> 18) & 63);
-                aBytes[nIdx++] = 128 + ((nChr >>> 12) & 63);
-                aBytes[nIdx++] = 128 + ((nChr >>> 6) & 63);
-                aBytes[nIdx++] = 128 + (nChr & 63);
-                nChrIdx++;
-            }
-            nChrIdx++;
         }
 
-        return aBytes;
+        const padding = inputIndexMod3 === 2
+            ? ''
+            : inputIndexMod3 === 1
+                ? '='
+                : '==';
+
+        return base64Output.substring(0, base64Output.length - 2 + inputIndexMod3) + padding;
     }
 }
 
