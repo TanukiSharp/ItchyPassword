dc2752bb90dc4606e18c7010a23fcb73e8e4a5e7
Sebastien <sebastien.saigo@gmail.com>
2026-02-24 14:04:02 +0100

    WIP

---
+++ src/ItchyPassword.Client/AppVersion.cs
@@ -0,0 +1,20 @@
+namespace ItchyPassword.Client;
+
+/// <summary>
+/// Application version constants.
+/// Major and Minor are updated manually. GitCommitHash is injected at build time via MSBuild.
+/// </summary>
+public static partial class AppVersion
+{
+    public const int Major = 2;
+    public const int Minor = 0;
+
+    public static string FullVersion
+    {
+        get
+        {
+            string hash = GitCommitHash.Length > 7 ? GitCommitHash[..7] : GitCommitHash;
+            return $"{Major}.{Minor}-{hash}";
+        }
+    }
+}
